93d9385630d7483a8dc43bdff279ce3ab16828ca
KyeongMin <93812148+KyeongMin5307@users.noreply.github.com>
2023-06-29 04:25:49 +0900

    Django Update
    Changed Django project's name into 'EasyWork'
    Updated README.md

--- Backend/EasyWork/asgi.py
+++ Backend/EasyWork/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for EasyResume project.
+ASGI config for EasyWork project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EasyResume.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EasyWork.settings')
 
 application = get_asgi_application()
--- Backend/EasyWork/urls.py
+++ Backend/EasyWork/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for EasyResume project.
+URL configuration for EasyWork project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/

d969a89c9e50fd9e1af9adce6a3b23a53e189a0b
dudtlstm <annpys1024@naver.com>
2023-06-29 14:35:57 +0900

    feat: 모델 및 시리얼라이저 수정

--- Backend/main/serializers.py
+++ Backend/main/serializers.py
@@ -5,3 +5,23 @@ class VoiceRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = VoiceRecord
         fields = ['voice']
+
+class ResumeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VoiceRecord
+        fields = [
+                'name', 
+                'birthday', 
+                'phone', 
+                'email', 
+                'address', 
+                'education', 
+                'career', 
+                'major_performance', 
+                'certificate', 
+                'prize', 
+                'ability', 
+                'military_service',
+                'cover_letter',
+                'why_apply',
+                ]

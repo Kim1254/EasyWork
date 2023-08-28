e719fd694ab5e412cd4584c43743fc1605b300b2
mayrang <pkss0626@naver.com>
2023-08-28 20:57:15 +0900

    이미지 업로드 구현

---
+++ Frontend/src/cloudinary/imageUpload.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const imageUpload = async (file: Blob) => {
+  try {
+    const formData = new FormData();
+    formData.append("api_key", process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY as string);
+
+    formData.append("timestamp", (Date.now() / 1000 || 0).toString());
+    formData.append("file", file);
+
+    formData.append("upload_preset", "react-shoppingmall");
+
+    const cloudName = process.env.NEXT_PUBLIC_CLOUD_NAME;
+
+    const result = await axios.post(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return result.data.url;
+  } catch (err) {
+    return null;
+  }
+};

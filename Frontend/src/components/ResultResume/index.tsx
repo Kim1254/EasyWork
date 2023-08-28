3436b957dd615773fc2b6045846a24e1676d9b10
mayrang <pkss0626@naver.com>
2023-08-28 22:02:06 +0900

    resultresume 변경

--- Frontend/src/components/ResultResume/index.tsx
+++ Frontend/src/components/ResultResume/index.tsx
@@ -11,12 +11,13 @@ const videoConstraints = {
   facingMode: "user",
 };
 
-export default async function ResultResume() {
+export default function ResultResume() {
   const [imageUrl, setImageUrl] = useState(null);
   const {
     state: { result },
     dispatch,
   } = useContext(ResultContext);
+  const [isWebcam, setIsWebcam] = useState(false);
   const pdf = makePdf(styles.pdf);
   const handlePdf = async () => {
     await pdf.viewWithPdf();
@@ -24,23 +25,36 @@ export default async function ResultResume() {
   const webcamRef = React.useRef<Webcam | null>(null);
   const capture = React.useCallback(async () => {
     const imageSrc = webcamRef.current?.getScreenshot();
-    console.log(imageSrc);
-    const url = await imageUpload(imageSrc);
+    const file = new File([imageSrc as string], "cam_picture.jpeg", { type: "image/jpeg" });
+    console.log(file);
+
+    const url = await imageUpload(file);
     setImageUrl(url);
+    console.log(url);
   }, [webcamRef]);
+  const addImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      console.log(e.target.files[0]);
+    }
+    // const url = await imageUpload(e.target.files);
+    // console.log(url);
+  };
   return (
     <section className={styles.section}>
       <div className={styles.pdf}></div>
-      <Webcam
-        audio={false}
-        height={720}
-        screenshotFormat="image/jpeg"
-        width={1280}
-        ref={webcamRef}
-        videoConstraints={videoConstraints}
-      />
+      {isWebcam && (
+        <Webcam
+          audio={false}
+          height={720}
+          screenshotFormat="image/jpeg"
+          width={1280}
+          ref={webcamRef}
+          videoConstraints={videoConstraints}
+        />
+      )}
       <button onClick={capture}>Capture photo</button>
       <button onClick={handlePdf}>pdf 만들기</button>
+      <input type="file" onChange={addImage} />
     </section>
   );
 }
